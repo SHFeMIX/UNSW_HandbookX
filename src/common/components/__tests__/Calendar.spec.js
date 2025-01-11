343b28fb92264c548fecd135fdcafcfcce32d135
SHFeMIX <alanshao_sh@foxmail.com>
2025-01-11 23:08:27 +0800

    日历组件单测

--- src/common/components/__tests__/Calendar.spec.js
+++ src/common/components/__tests__/Calendar.spec.js
@@ -33,3 +33,42 @@ describe('default rendering time now', () => {
     expect(wrapper.get('*[style^="background:"]').text()).toBe(currentDate.date().toString())
   })
 })
+
+describe('rendering time now - n month', () => {
+  const wrapper = mount(Calendar)
+  const leftButton = wrapper.get('.header> :first-child')
+  it('rendered left button properly', () => {
+    expect(leftButton.text()).toBe('<')
+  })
+
+
+  const times = Math.floor(Math.random() * 10)
+  for (let i = 1; i <= times; i ++) {
+    leftButton.trigger('click')
+  }
+  const currentDate = dayjs().add(-times, 'month')
+
+  it('rendered year and month properly in ".title" element', () => testTitle(wrapper, currentDate))
+
+  it('rendered days properly', () => testDays(wrapper, currentDate))
+
+})
+
+describe('rendering time now + n month', () => {
+  const wrapper = mount(Calendar)
+  const rightButton = wrapper.get('.header> :last-child')
+  it('rendered left button properly', () => {
+    expect(rightButton.text()).toBe('>')
+  })
+
+
+  const times = Math.floor(Math.random() * 10)
+  for (let i = 1; i <= times; i ++) {
+    rightButton.trigger('click')
+  }
+  const currentDate = dayjs().add(times, 'month')
+
+  it('rendered year and month properly in ".title" element', () => testTitle(wrapper, currentDate))
+
+  it('rendered days properly', () => testDays(wrapper, currentDate))
+})
